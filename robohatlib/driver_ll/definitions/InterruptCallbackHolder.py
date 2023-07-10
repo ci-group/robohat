8cac41e6b7ef3d064ac92e2239554263aa12d322
Arnoud Denker <a.denker@vu.nl>
2023-07-10 17:00:58 +0200

    Added comments at InterruptCallbackHolder.py

--- robohatlib/driver_ll/definitions/InterruptCallbackHolder.py
+++ robohatlib/driver_ll/definitions/InterruptCallbackHolder.py
@@ -33,12 +33,22 @@ class InterruptCallbackHolder:
     #--------------------------------------------------------------------------------------
 
     def set_release_int_function(self, _release_int_function) -> None:
+        """
+        The interrupt reset function can be overriden by a new function
+        @param _release_int_function: the new fucntion
+        @return: None
+        """
         self.__release_int_function = _release_int_function
 
     #--------------------------------------------------------------------------------------
     #--------------------------------------------------------------------------------------
     #--------------------------------------------------------------------------------------
-    def set_time_between_new_possible_trigger_in_ms(self, _time_between_new_possible_trigger) -> None:
+    def set_time_between_new_possible_trigger_in_ms(self, _time_between_new_possible_trigger:int = 0) -> None:
+        """
+        The minimal time between 2 triggers. 0 = default
+        @param _time_between_new_possible_trigger:
+        @return: None
+        """
         self.__time_between_new_possible_trigger = _time_between_new_possible_trigger * 1000000
 
     #--------------------------------------------------------------------------------------
@@ -46,17 +56,19 @@ class InterruptCallbackHolder:
     #--------------------------------------------------------------------------------------
 
     def execute_callback(self, _io_nr:int) -> None:
+        """
+        This function will call the reset the interrupt and executes callback
+
+        @param _io_nr: io nr which triggered the interrupt
+        @return: None
+        """
         if self.__release_int_function is not None:
             self.__release_int_function(_io_nr)
 
         diff_trigger_time = time.time_ns() - self.__last_time_triggered
-
         if diff_trigger_time > self.__time_between_new_possible_trigger:
             if self.__callback is not None:
-                print("diff: " + str(diff_trigger_time) + " compare: " + str(self.__time_between_new_possible_trigger) )
                 self.__last_time_triggered = time.time_ns()
-                print("last: " + str(self.__last_time_triggered) )
-
                 self.__callback(_io_nr)
 
     #--------------------------------------------------------------------------------------
