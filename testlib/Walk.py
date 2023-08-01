174323995ab846e81c19c71e4ec1304d11f8b2d0
Arnoud Denker <a.denker@vu.nl>
2023-08-01 15:05:48 +0200

    Made walk routine slower. Movement in separate thread

--- testlib/Walk.py
+++ testlib/Walk.py
@@ -7,12 +7,11 @@ try:
     from enum import IntEnum
 
     from testlib.WalkDriver import WalkDriver
+    from testlib.WalkServoID import WalkServoID
 
 except ImportError:
     print("Failed to import needed dependencies for the Walk class")
     raise
-
-
 
 
 # --------------------------------------------------------------------------------------
@@ -44,30 +43,19 @@ RIGHT_BACK_LEG_SERVO_NR = 5
 TIME_BETWEEN_STEP = 5
 
 LEG_NEUTRAL = 45.0
-LEG_DOWN = 10
+LEG_DOWN = 10.0
 LEG_UP = 90.0
 
 NECK_NEUTRAL = 90.0
-NECK_LEFT = 80.0
-NECK_RIGHT = 100.0
+NECK_LEFT = 60.0
+NECK_RIGHT = 120.0
 
-HIP_NEUTRAL = 80.0
-HIP_LEFT = 30.0
-HIP_RIGHT = 150.0
+HIP_NEUTRAL = 90.0
+HIP_LEFT = 70.0
+HIP_RIGHT = 110.0
 
+DELAY_STEP = 1
 
-
-
-class ID(IntEnum):
-    """
-    Internal ID. Maps to an array. ID nr should be lower than the size of the array ( < 32 )
-    """
-    LEFT_FRONT_LEG = 0
-    RIGHT_FRONT_LEG = 1
-    NECK = 2
-    HIP = 3
-    LEFT_BACK_LEG = 4
-    RIGHT_BACK_LEG = 5
 
     # --------------------------------------------------------------------------------------
     # --------------------------------------------------------------------------------------
@@ -90,43 +78,59 @@ class Walk:
         """
         self.__robohat = _robohat
 
-        self.__servo_positions = [90.0] * 32
+        self.__walk_driver = WalkDriver(_robohat)
 
-        self.set_servo_preset_value(ID.LEFT_FRONT_LEG, LEG_NEUTRAL)
-        self.set_servo_preset_value(ID.RIGHT_FRONT_LEG, LEG_NEUTRAL)
-        self.set_servo_preset_value(ID.NECK, NECK_NEUTRAL)
-        self.set_servo_preset_value(ID.HIP, NECK_NEUTRAL)
-        self.set_servo_preset_value(ID.LEFT_BACK_LEG, LEG_NEUTRAL)
-        self.set_servo_preset_value(ID.RIGHT_BACK_LEG, LEG_NEUTRAL)
+        self.__set_servo_preset_value(WalkServoID.LEFT_FRONT_LEG, LEG_NEUTRAL)
+        self.__set_servo_preset_value(WalkServoID.RIGHT_FRONT_LEG, LEG_NEUTRAL)
+        self.__set_servo_preset_value(WalkServoID.NECK, NECK_NEUTRAL)
+        self.__set_servo_preset_value(WalkServoID.HIP, NECK_NEUTRAL)
+        self.__set_servo_preset_value(WalkServoID.LEFT_BACK_LEG, LEG_NEUTRAL)
+        self.__set_servo_preset_value(WalkServoID.RIGHT_BACK_LEG, LEG_NEUTRAL)
 
-        self.__walk_driver = WalkDriver()
-        self.__walk_driver.start_walking()
+        self.__running = False
 
 
-        #self.push_preset_values_to_servos()
 
     # --------------------------------------------------------------------------------------
     # --------------------------------------------------------------------------------------
     # --------------------------------------------------------------------------------------
 
     def start_walking(self) -> None:
-        """
-        The Robo hat will start walking
+        """!
+        The Robohat will start walking
         @return: None
         """
+
         print("Started walking")
 
-        running = True
-        counter = 0
+        self.__walk_driver.start_walking()
 
-        while running:
+        counter = 0
+        self.__running = True
+
+        while self.__running:
             self._step_forward_right()
             self._step_forward_left()
 
             counter = counter + 1
             if counter > 10:
-                running = False
+                self.__running = False
 
+        self.stop_walking()
+
+
+    # --------------------------------------------------------------------------------------
+    # --------------------------------------------------------------------------------------
+    # --------------------------------------------------------------------------------------
+
+    def stop_walking(self) -> None:
+        """!
+        The Robohat will stop with walking
+        @return: None
+        """
+
+        self.__walk_driver.stop_walking()
+        self.__running = False
         print("Stopped walking")
 
     # --------------------------------------------------------------------------------------
@@ -135,75 +139,72 @@ class Walk:
 
     def _step_forward_right(self):
         self._leg_right_front_up()
-        self._joint_right()
-        self._leg_right_front_neutral()
-        self._joint_neutral()
-        self._leg_left_back_up()
-        self._joint_left()
-        self._leg_left_back_neutral()
-        self._joint_neutral()
+        time.sleep(DELAY_STEP)
 
-        print("STALL")
-        time.sleep(TIME_BETWEEN_STEP)
+        self._joint_left()
+        time.sleep(DELAY_STEP)
+
+        self._leg_right_front_neutral()
+        time.sleep(DELAY_STEP)
+
+        self._joint_neutral()
+        time.sleep(DELAY_STEP)
+
+        self._leg_left_back_up()
+        time.sleep(DELAY_STEP)
+
+        self._joint_right()
+        time.sleep(DELAY_STEP)
+
+        self._leg_left_back_neutral()
+        time.sleep(DELAY_STEP)
+
+        self._joint_neutral()
+        time.sleep(DELAY_STEP)
+
+    # --------------------------------------------------------------------------------------
+    # --------------------------------------------------------------------------------------
     # --------------------------------------------------------------------------------------
 
     def _step_forward_left(self):
         self._leg_left_front_up()
-        self._joint_left()
-        self._leg_left_front_neutral()
-        self._joint_neutral()
-        self._leg_right_back_up()
+        time.sleep(DELAY_STEP)
+
         self._joint_right()
-        self._leg_right_back_neutral()
+        time.sleep(DELAY_STEP)
+
+        self._leg_left_front_neutral()
+        time.sleep(DELAY_STEP)
+
         self._joint_neutral()
+        time.sleep(DELAY_STEP)
 
-        print("STALL")
-        time.sleep(TIME_BETWEEN_STEP)
-    # --------------------------------------------------------------------------------------
+        self._leg_right_back_up()
+        time.sleep(DELAY_STEP)
 
-    # def _step_backward_right(self):
-    #
-    #     time.sleep(TIME_BETWEEN_STEP)
-    # # --------------------------------------------------------------------------------------
-    #
-    # def _step_backward_left(self):
-    #
-    #     time.sleep(TIME_BETWEEN_STEP)
+        self._joint_left()
+        time.sleep(DELAY_STEP)
 
-    # --------------------------------------------------------------------------------------
-    # --------------------------------------------------------------------------------------
-    # --------------------------------------------------------------------------------------
+        self._leg_right_back_neutral()
+        time.sleep(DELAY_STEP)
 
-    def push_preset_values_to_servos(self) -> None:
-        unused_angle = 0.0
+        self._joint_neutral()
+        time.sleep(DELAY_STEP)
 
-        angles = [unused_angle] * 32
-        angles[LEFT_FRONT_LEG_SERVO_NR] =        self.get_servo_preset_value(ID.LEFT_FRONT_LEG)
-        angles[RIGHT_FRONT_LEG_SERVO_NR] =       self.get_servo_preset_value(ID.RIGHT_FRONT_LEG)
-        angles[NECK_SERVO_NR] =                  self.get_servo_preset_value(ID.NECK)
-        angles[HIP_SERVO_NR] =                   self.get_servo_preset_value(ID.HIP)
-        angles[LEFT_BACK_LEG_SERVO_NR] =         self.get_servo_preset_value(ID.LEFT_BACK_LEG)
-        angles[RIGHT_BACK_LEG_SERVO_NR] =        self.get_servo_preset_value(ID.RIGHT_BACK_LEG)
-
-        self.__robohat.set_servo_multiple_angles(angles)
 
     # --------------------------------------------------------------------------------------
     # --------------------------------------------------------------------------------------
     # --------------------------------------------------------------------------------------
 
-    def set_servo_preset_value(self, _servo_id:ID, _pos:float) -> None:
-        array_pos_servo_pos:int = int(_servo_id)
-
-        self.__servo_positions[array_pos_servo_pos] = _pos
+    def __set_servo_preset_value(self, _servo_id:WalkServoID, _pos:float) -> None:
+        self.__walk_driver.set_servo_preset_value(_servo_id, _pos)
 
     # --------------------------------------------------------------------------------------
     # --------------------------------------------------------------------------------------
     # --------------------------------------------------------------------------------------
 
-    def get_servo_preset_value(self, _servo_id:ID) -> float:
-        array_pos_servo_pos:int = int(_servo_id)
-
-        return self.__servo_positions[array_pos_servo_pos]
+    def __get_servo_is_wanted_angle(self, _servo_id:WalkServoID) -> bool:
+        return self.__walk_driver.get_servo_is_wanted_angle(_servo_id)
 
     # --------------------------------------------------------------------------------------
     # --------------------------------------------------------------------------------------
@@ -211,144 +212,159 @@ class Walk:
 
     def _leg_right_front_up(self):
         print("_leg_right_front_up")
-        self.set_servo_preset_value(ID.RIGHT_FRONT_LEG, LEG_UP)
-        self.push_preset_values_to_servos()
-        time.sleep(1)
+        self.__set_servo_preset_value(WalkServoID.RIGHT_FRONT_LEG, LEG_UP)
+        while self.__get_servo_is_wanted_angle(WalkServoID.RIGHT_FRONT_LEG) is False:
+            time.sleep(0.1)
 
     def _leg_right_front_down(self):
         print("_leg_right_front_down")
-        self.set_servo_preset_value(ID.RIGHT_FRONT_LEG, LEG_DOWN)
-        self.push_preset_values_to_servos()
-        time.sleep(1)
+        self.__set_servo_preset_value(WalkServoID.RIGHT_FRONT_LEG, LEG_DOWN)
+        while self.__get_servo_is_wanted_angle(WalkServoID.RIGHT_FRONT_LEG) is False:
+            time.sleep(0.1)
 
     def _leg_right_front_neutral(self):
         print("_leg_right_front_neutral")
-        self.set_servo_preset_value(ID.RIGHT_FRONT_LEG, LEG_NEUTRAL)
-        self.push_preset_values_to_servos()
-        time.sleep(1)
+        self.__set_servo_preset_value(WalkServoID.RIGHT_FRONT_LEG, LEG_NEUTRAL)
+        while self.__get_servo_is_wanted_angle(WalkServoID.RIGHT_FRONT_LEG) is False:
+            print("_leg_right_front_neutral zzz")
+            time.sleep(0.1)
 
+    # --------------------------------------------------------------------------------------
+    # --------------------------------------------------------------------------------------
     # --------------------------------------------------------------------------------------
 
     def _leg_left_front_up(self):
         print("_leg_left_front_up")
-        self.set_servo_preset_value(ID.LEFT_FRONT_LEG, LEG_UP)
-        self.push_preset_values_to_servos()
-        time.sleep(1)
+        self.__set_servo_preset_value(WalkServoID.LEFT_FRONT_LEG, LEG_UP)
+        while self.__get_servo_is_wanted_angle(WalkServoID.LEFT_FRONT_LEG) is False:
+            time.sleep(0.1)
+
 
     def _leg_left_front_down(self):
         print("_leg_left_front_down")
-        self.set_servo_preset_value(ID.LEFT_FRONT_LEG, LEG_DOWN)
-        self.push_preset_values_to_servos()
-        time.sleep(1)
+        self.__set_servo_preset_value(WalkServoID.LEFT_FRONT_LEG, LEG_DOWN)
+        while self.__get_servo_is_wanted_angle(WalkServoID.LEFT_FRONT_LEG) is False:
+            time.sleep(0.1)
 
     def _leg_left_front_neutral(self):
         print("_leg_left_front_neutral")
-        self.set_servo_preset_value(ID.LEFT_FRONT_LEG, LEG_NEUTRAL)
-        self.push_preset_values_to_servos()
-        time.sleep(1)
+        self.__set_servo_preset_value(WalkServoID.LEFT_FRONT_LEG, LEG_NEUTRAL)
+        while self.__get_servo_is_wanted_angle(WalkServoID.LEFT_FRONT_LEG) is False:
+            time.sleep(0.1)
 
+    # --------------------------------------------------------------------------------------
+    # --------------------------------------------------------------------------------------
     # --------------------------------------------------------------------------------------
 
     def _leg_right_back_up(self):
         print("_leg_right_back_up")
-        self.set_servo_preset_value(ID.RIGHT_BACK_LEG, LEG_UP)
-        self.push_preset_values_to_servos()
-        time.sleep(1)
+        self.__set_servo_preset_value(WalkServoID.RIGHT_BACK_LEG, LEG_UP)
+        while self.__get_servo_is_wanted_angle(WalkServoID.RIGHT_BACK_LEG) is False:
+            time.sleep(0.1)
 
     def _leg_right_back_down(self):
         print("_leg_right_back_down")
-        self.set_servo_preset_value(ID.RIGHT_BACK_LEG, LEG_DOWN)
-        self.push_preset_values_to_servos()
-        time.sleep(1)
+        self.__set_servo_preset_value(WalkServoID.RIGHT_BACK_LEG, LEG_DOWN)
+        while self.__get_servo_is_wanted_angle(WalkServoID.RIGHT_BACK_LEG) is False:
+            time.sleep(0.1)
 
     def _leg_right_back_neutral(self):
         print("_leg_right_back_neutral")
-        self.set_servo_preset_value(ID.RIGHT_BACK_LEG, LEG_NEUTRAL)
-        self.push_preset_values_to_servos()
-        time.sleep(1)
+        self.__set_servo_preset_value(WalkServoID.RIGHT_BACK_LEG, LEG_NEUTRAL)
+        while self.__get_servo_is_wanted_angle(WalkServoID.RIGHT_BACK_LEG) is False:
+            time.sleep(0.1)
 
+    # --------------------------------------------------------------------------------------
+    # --------------------------------------------------------------------------------------
     # --------------------------------------------------------------------------------------
 
     def _leg_left_back_up(self):
         print("_leg_left_back_up")
-        self.set_servo_preset_value(ID.LEFT_BACK_LEG, LEG_UP)
-        self.push_preset_values_to_servos()
-        time.sleep(1)
+        self.__set_servo_preset_value(WalkServoID.LEFT_BACK_LEG, LEG_UP)
+        while self.__get_servo_is_wanted_angle(WalkServoID.LEFT_BACK_LEG) is False:
+            time.sleep(0.1)
 
     def _leg_left_back_down(self):
         print("_leg_left_back_down")
-        self.set_servo_preset_value(ID.LEFT_BACK_LEG, LEG_DOWN)
-        self.push_preset_values_to_servos()
-        time.sleep(1)
+        self.__set_servo_preset_value(WalkServoID.LEFT_BACK_LEG, LEG_DOWN)
+        while self.__get_servo_is_wanted_angle(WalkServoID.LEFT_BACK_LEG) is False:
+            time.sleep(0.1)
 
     def _leg_left_back_neutral(self):
         print("_leg_left_back_neutral")
-        self.set_servo_preset_value(ID.LEFT_BACK_LEG, LEG_NEUTRAL)
-        self.push_preset_values_to_servos()
-        time.sleep(1)
+        self.__set_servo_preset_value(WalkServoID.LEFT_BACK_LEG, LEG_NEUTRAL)
+        while self.__get_servo_is_wanted_angle(WalkServoID.LEFT_BACK_LEG) is False:
+            time.sleep(0.1)
 
+    # --------------------------------------------------------------------------------------
+    # --------------------------------------------------------------------------------------
     # --------------------------------------------------------------------------------------
 
     def _joint_right(self):
         print("_joint_right")
-        self.set_servo_preset_value(ID.NECK, NECK_RIGHT)
-        self.set_servo_preset_value(ID.HIP, HIP_LEFT)
-
-        self.push_preset_values_to_servos()
-        time.sleep(1)
+        self.__set_servo_preset_value(WalkServoID.NECK, NECK_RIGHT)
+        self.__set_servo_preset_value(WalkServoID.HIP, HIP_LEFT)
+        while self.__get_servo_is_wanted_angle(WalkServoID.NECK) is False or self.__get_servo_is_wanted_angle(WalkServoID.HIP) is False:
+            time.sleep(0.1)
 
     def _joint_left(self):
         print("_joint_left")
-        self.set_servo_preset_value(ID.NECK, NECK_LEFT)
-        self.set_servo_preset_value(ID.HIP, HIP_RIGHT)
-
-        self.push_preset_values_to_servos()
-        time.sleep(1)
+        self.__set_servo_preset_value(WalkServoID.NECK, NECK_LEFT)
+        self.__set_servo_preset_value(WalkServoID.HIP, HIP_RIGHT)
+        while self.__get_servo_is_wanted_angle(WalkServoID.NECK) is False or self.__get_servo_is_wanted_angle(WalkServoID.HIP) is False:
+            time.sleep(0.1)
 
     def _joint_neutral(self):
         print("_joint_neutral")
-        self.set_servo_preset_value(ID.NECK, NECK_NEUTRAL)
-        self.set_servo_preset_value(ID.HIP, NECK_NEUTRAL)
-        self.push_preset_values_to_servos()
-        time.sleep(1)
+        self.__set_servo_preset_value(WalkServoID.NECK, NECK_NEUTRAL)
+        self.__set_servo_preset_value(WalkServoID.HIP, NECK_NEUTRAL)
+        while self.__get_servo_is_wanted_angle(WalkServoID.NECK) is False or self.__get_servo_is_wanted_angle(WalkServoID.HIP) is False:
+            time.sleep(0.1)
 
+    # --------------------------------------------------------------------------------------
+    # --------------------------------------------------------------------------------------
     # --------------------------------------------------------------------------------------
 
     def _neck_right(self):
         print("_neck_right")
-        self.set_servo_preset_value(ID.NECK, NECK_RIGHT)
-        self.push_preset_values_to_servos()
-        time.sleep(1)
+        self.__set_servo_preset_value(WalkServoID.NECK, NECK_RIGHT)
+        while self.__get_servo_is_wanted_angle(WalkServoID.NECK) is False or self.__get_servo_is_wanted_angle(WalkServoID.HIP) is False:
+            time.sleep(0.1)
 
     def _neck_left(self):
         print("_neck_left")
-        self.set_servo_preset_value(ID.NECK, NECK_LEFT)
-        self.push_preset_values_to_servos()
-        time.sleep(1)
+        self.__set_servo_preset_value(WalkServoID.NECK, NECK_LEFT)
+        while self.__get_servo_is_wanted_angle(WalkServoID.NECK) is False or self.__get_servo_is_wanted_angle(WalkServoID.HIP) is False:
+            time.sleep(0.1)
 
     def _neck_neutral(self):
         print("_neck_neutral")
-        self.set_servo_preset_value(ID.NECK, NECK_NEUTRAL)
-        self.push_preset_values_to_servos()
-        time.sleep(1)
+        self.__set_servo_preset_value(WalkServoID.NECK, NECK_NEUTRAL)
+        while self.__get_servo_is_wanted_angle(WalkServoID.NECK) is False or self.__get_servo_is_wanted_angle(WalkServoID.HIP) is False:
+            time.sleep(0.1)
 
+    # --------------------------------------------------------------------------------------
+    # --------------------------------------------------------------------------------------
     # --------------------------------------------------------------------------------------
 
     def _hip_right(self):
         print("_hip_right")
-        self.set_servo_preset_value(ID.HIP, HIP_RIGHT)
-        self.push_preset_values_to_servos()
-        time.sleep(1)
+        self.__set_servo_preset_value(WalkServoID.HIP, HIP_RIGHT)
+        while self.__get_servo_is_wanted_angle(WalkServoID.NECK) is False or self.__get_servo_is_wanted_angle(WalkServoID.HIP) is False:
+            time.sleep(0.1)
 
     def _hip_left(self):
         print("_hip_left")
-        self.set_servo_preset_value(ID.HIP, HIP_LEFT)
-        self.push_preset_values_to_servos()
-        time.sleep(1)
+        self.__set_servo_preset_value(WalkServoID.HIP, HIP_LEFT)
+        while self.__get_servo_is_wanted_angle(WalkServoID.NECK) is False or self.__get_servo_is_wanted_angle(WalkServoID.HIP) is False:
+            time.sleep(0.1)
 
     def _hip_neutral(self):
         print("_hip_neutral")
-        self.set_servo_preset_value(ID.HIP, HIP_NEUTRAL)
-        self.push_preset_values_to_servos()
-        time.sleep(1)
+        self.__set_servo_preset_value(WalkServoID.HIP, HIP_NEUTRAL)
+        while self.__get_servo_is_wanted_angle(WalkServoID.NECK) is False or self.__get_servo_is_wanted_angle(WalkServoID.HIP) is False:
+            time.sleep(0.1)
 
+    # --------------------------------------------------------------------------------------
+    # --------------------------------------------------------------------------------------
+    # --------------------------------------------------------------------------------------
--- testlib/WalkDriver.py
+++ testlib/WalkDriver.py
@@ -1,3 +1,4 @@
+
 try:
     from robohatlib.Robohat import Robohat
     import time
@@ -5,6 +6,7 @@ try:
     import time
     from enum import Enum
     from enum import IntEnum
+    from testlib.WalkServoID import WalkServoID
 
 except ImportError:
     print("Failed to import needed dependencies for the WalkDriver class")
@@ -16,12 +18,16 @@ except ImportError:
 
 class WalkDriver:
 
-    def __init__(self):
+    def __init__(self, _robohat:Robohat):
         """!
          Constructor
         """
         print("Constructor of WalkDriver")
+        self.__robohat = _robohat
+
         self.__running = False
+        self.__preset_servo_positions = [90.0] * 32
+        self.__current_servo_positions = [90.0] * 32
 
     # --------------------------------------------------------------------------------------
     # --------------------------------------------------------------------------------------
@@ -50,12 +56,53 @@ class WalkDriver:
 
     def run(self):
         while self.__running is True:
-            # Do something
-            print('Doing something important in the background')
+            length:int = len(self.__preset_servo_positions)
 
-            time.sleep(1)  # wait 100 mS
+            for servo_nr in range( length ):
+                diff = self.__preset_servo_positions[servo_nr] - self.__current_servo_positions[servo_nr]
+                if diff < 0:
+                    self.__current_servo_positions[servo_nr] = self.__current_servo_positions[servo_nr] - 1
+                elif diff > 0:
+                    self.__current_servo_positions[servo_nr] = self.__current_servo_positions[servo_nr] + 1
+
+            self.__robohat.set_servo_multiple_angles(self.__current_servo_positions)
+
+            time.sleep(0.001)  # wait 1 mS
 
     # --------------------------------------------------------------------------------------
     # --------------------------------------------------------------------------------------
     # --------------------------------------------------------------------------------------
 
+    def get_servo_is_wanted_angle(self, _servo_id:WalkServoID) -> bool:
+        servo_nr: int = int(_servo_id)
+        angle_preset = self.__preset_servo_positions[servo_nr]
+
+        angle_low = angle_preset
+        angle_high = angle_preset + 1
+
+        #print(str(angle_low ) + " < " + str(angle_preset) + " < " + str(angle_high) )
+        if angle_preset >= angle_low and angle_preset <= angle_high:
+            return True
+        else:
+            return False
+
+    # --------------------------------------------------------------------------------------
+    # --------------------------------------------------------------------------------------
+    # --------------------------------------------------------------------------------------
+
+    def set_servo_preset_value(self, _servo_id: WalkServoID, _pos: float) -> None:
+        array_pos_servo_pos: int = int(_servo_id)
+        self.__preset_servo_positions[array_pos_servo_pos] = _pos
+
+    # --------------------------------------------------------------------------------------
+    # --------------------------------------------------------------------------------------
+    # --------------------------------------------------------------------------------------
+
+    def __get_servo_preset_value(self, _servo_id:WalkServoID) -> float:
+        array_pos_servo_pos:int = int(_servo_id)
+
+        return self.__preset_servo_positions[array_pos_servo_pos]
+
+    # --------------------------------------------------------------------------------------
+    # --------------------------------------------------------------------------------------
+    # --------------------------------------------------------------------------------------
