d8d8afeb6f15fa1e954d50fb5e40db606972b19e
Arnoud Denker <a.denker@vu.nl>
2023-08-04 11:53:05 +0200

    Alarm is permitted

--- robohatlib/RobohatConfig.py
+++ robohatlib/RobohatConfig.py
@@ -32,7 +32,7 @@ DEBUG = True                                       # by changing this value to T
 Accu settings
 """
 
-ALARM_PERMITTED = False                              # sound an alarm when a system alert is present, such as to low accu capacity
+ALARM_PERMITTED = True                              # sound an alarm when a system alert is present, such as to low accu capacity
 ALARM_TIMEOUT_IN_SEC = 300                          # timeout between alarm
 INIT_BEEP_PERMITTED = False                         # beep when started if True
 
