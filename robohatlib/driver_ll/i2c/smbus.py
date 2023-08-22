ad3c1b55a67d0a3aea7a1e5e02c2381efc32e4b8
Arnoud Denker <a.denker@vu.nl>
2023-08-22 09:12:46 +0200

    Added Copyright message to all source files

--- robohatlib/driver_ll/i2c/smbus.py
+++ robohatlib/driver_ll/i2c/smbus.py
@@ -1,3 +1,10 @@
+"""!
+Robohatlib (2022-2823-01)
+Copyright © 2023 Vrije Universiteit Amsterdam
+Electronica-Beta-VU
+A. Denker (a.denker@vu.nl)
+"""
+
 try:
     from fcntl import ioctl
     from ctypes import c_uint32, c_uint8, c_uint16, pointer, POINTER, Structure, Array, Union
