9a5b1640029bdafc250407694f123f3957d0de77
Arnoud Denker <a.denker@vu.nl>
2023-08-23 15:27:07 +0200

    Updated documentation

--- robohatlib/RobohatConstants.py
+++ robohatlib/RobohatConstants.py
@@ -9,6 +9,6 @@ do not alter
 """
 
 ROBOHAT_LIB_VERSION_STR = "0.2.0"                    # version nr of library
-ROBOHAT_BUILD_DATE_STR = "15-aug-2023"               # build date of library YYMMDD
+ROBOHAT_BUILD_DATE_STR = "23-aug-2023"               # build date of library YYMMDD
 
 
