6c105a72fbcf0fc9523bea30cc917b72d65fb6b6
Arnoud Denker <a.denker@vu.nl>
2024-01-02 12:18:34 +0100

    Bump to 0.4.4

--- robohatlib/RobohatConstants.py
+++ robohatlib/RobohatConstants.py
@@ -8,10 +8,12 @@ This file consists of the Constants for the Robohat lib
 do not alter
 """
 
-ROBOHAT_LIB_VERSION_STR = "0.4.3"                    # version nr of library
-ROBOHAT_BUILD_DATE_STR = "7-dec-2023"               # build date of library YYMMDD
+ROBOHAT_LIB_VERSION_STR = "0.4.4"                    # version nr of library
+ROBOHAT_BUILD_DATE_STR = "2-jan-2024"               # build date of library YYMMDD
 
 
 """
+0.4.4 DEFAULT_DELAY_BETWEEN_ACTION mismatch between DEFAULT_SERVO_UPDATE_VALUE
+
 0.4.0 was from 30-aug-2023
 """
